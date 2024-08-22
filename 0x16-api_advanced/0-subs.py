f91c51cce4a86bffcf1ab437c1756edd43cb9d19
David Adesanya <jesugbohunmi23@gmail.com>
2024-08-22 02:24:07 +0100

    Refactor number_of_subscribers function to handle invalid subreddits gracefully

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-"""
-Fetch the number of subscribers for a specified subreddit
-"""
-
-import requests
+"""Module to fetch subreddit subscriber count"""
 
 
 def number_of_subscribers(subreddit):
-    """
-    This function queries the Reddit API to retrieve the number of subscribers
-    (total subscribers, not active users) for a specified subreddit.
-    """
+    """Fetches the number of subscribers for a given subreddit using Reddit API"""
+    import requests
 
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Mozilla/5.0'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     
-    try:
-        response = requests.get(url, headers=user_agent, allow_redirects=False)
-        
-        if response.status_code == 200:
-            results = response.json()
-            return results.get('data', {}).get('subscribers', 0)
-        else:
-            return 0
-    except requests.RequestException:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code != 200:
         return 0
+
+    data = response.json().get("data", {})
+    return data.get("subscribers", 0)
